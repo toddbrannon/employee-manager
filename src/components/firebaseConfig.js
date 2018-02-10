90b3d798096f38c7d147f7fa03535d1ab3eb58ad
toddbrannon <toddqbrannon@gmail.com>
2018-02-10 17:32:25 +0000

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyDYw3miY5jmAegxvlGAVxuXIiFX2bBfArY",
+    authDomain: "vuefs-dev-a9d0b.firebaseapp.com",
+    databaseURL: "https://vuefs-dev-a9d0b.firebaseio.com",
+    projectId: "vuefs-dev-a9d0b",
+    storageBucket: "vuefs-dev-a9d0b.appspot.com",
+    messagingSenderId: "473548487727"
+};
