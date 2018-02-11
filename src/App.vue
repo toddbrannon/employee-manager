e5e3ad63a5816ae89cfa1e6f1d4c2478e111f30e
Todd Brannon <toddqbrannon@gmail.com>
2018-02-11 10:38:07 -0600

    add firebase connections

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Navbar />
+    <div class="container">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+    import Navbar from './components/Navbar'
+    export default {
+        name: 'App',
+        components: {
+            Navbar
+        }
+    }
 </script>
-
-
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,16 +1,48 @@
 <template>
     <div id="dashboard">
-        <h3>Dashboard</h3>
+        <ul class="collection with-header">
+            <li class="collection-header">
+                <h4>Emloyees</h4>
+            </li>
+            <li v-for="employee in employees" v-bind:key="employee.id" class="collection-item">
+                <div class="chip">{{employee.dept}}</div>
+                {{employee.employee_id}}:{{employee.name}}
+
+                <router-link class="secondary-content" v-bind:to="{name: 'view-employee', params: {employee_id: employee.employee_id}}">
+                    <i class="fa fa-eye"></i>
+                </router-link>  
+            </li>
+        </ul>
+        <div class="fixed-action-btn">
+            <router-link to="/new" class="btn-floating btn-large red">
+                <i class="fa fa-plus"></i>
+            </router-link>
+        </div>
     </div>
 </template>
 
 <script>
-export default  {
-    name:'dashboard',
-    data() {
-        return {
-            
+    import db from './firebaseInit'
+    export default {
+        name: 'dashboard',
+        data() {
+            return {
+                employees: []
+            }
+        },
+        created() {
+            db.collection('employees').orderBy('dept').get().then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    const data = {
+                        'id': doc.id,
+                        'employee_id': doc.data().employee_id,
+                        'name': doc.data().name,
+                        'dept': doc.data().dept,
+                        'position': doc.data().position
+                    }
+                    this.employees.push(data)
+                })
+            })
         }
     }
-}
 </script>
